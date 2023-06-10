6e2c0dfbde9a9bcd180420860e74f27e95169a2e
dexpacito <e0957612@u.nus.edu>
2023-06-10 19:28:27 +0800

    add menubar and buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="menuBar">
+        <div className="leftContent">
+          <h1>parkingkorkor Menu Bar WIP!</h1>
+        </div>
+        <div className="rightContent">
+          <button>Log In</button>
+          <button>Sign Up</button>
+        </div>
+      </div>
       <header className="App-header">
         <p>Hello World!</p>
       </header>
@@ -12,3 +20,4 @@ function App() {
 }
 
 export default App;
+
