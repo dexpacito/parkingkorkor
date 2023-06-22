0fa7dc16f91adf34142595c71148ed7e163c9b54
Nicholas Lee <niclee1219@gmail.com>
2023-06-22 21:12:22 +0800

    fixed firebase permissions

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -10,15 +10,20 @@ function Dashboard() {
   const navigate = useNavigate();
   const fetchUserName = async () => {
     try {
-      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-      const doc = await getDocs(q);
-      const data = doc.docs[0].data();
-      setName(data.name);
+      if (user && user.uid) {
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const doc = await getDocs(q);
+        if (doc.docs.length > 0) {
+          const data = doc.docs[0].data();
+          setName(data.name);
+        }
+      }
     } catch (err) {
       console.error(err);
-      alert("An error occured while fetching user data");
+      alert("An error occurred while fetching user data :(");
     }
   };
+
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
@@ -26,15 +31,15 @@ function Dashboard() {
   }, [user, loading, navigate]);
   return (
     <div className="dashboard">
-       <div className="dashboard__container">
+      <div className="dashboard__container">
         Logged in as
-         <div>{name}</div>
-         <div>{user?.email}</div>
-         <button className="dashboard__btn" onClick={logout}>
+        <div>{name}</div>
+        <div>{user?.email}</div>
+        <button className="dashboard__btn" onClick={logout}>
           Logout
-         </button>
-       </div>
-     </div>
+        </button>
+      </div>
+    </div>
   );
 }
 export default Dashboard;
