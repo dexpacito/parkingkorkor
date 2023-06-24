4113cc6dd8937e70f606e6ac96ff924ea322f04e
Nicholas Lee <niclee1219@gmail.com>
2023-06-25 00:16:17 +0800

    added text above

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -6,6 +6,10 @@ import Map from "./Map";
 function Home() {
   return (
     <div className="home-container">
+      <p className="Home-heading">Park Smarter, Not Harder</p>
+      <p className="Home-text">
+        Let Parkingkorkor find the best parking solutions for you.
+      </p>
       <Map />
       <Footer />
     </div>
