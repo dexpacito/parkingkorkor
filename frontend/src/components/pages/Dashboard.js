a7100b0b7dcdc04ddf3ca6b808be5f2f17491600
Nicholas Lee <niclee1219@gmail.com>
2023-07-14 15:34:27 +0800

    added ability to edit name and add bio

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -3,46 +3,127 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import "../../App.css";
 import { auth, db, logout } from "./firebase";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import {
+  query,
+  collection,
+  getDocs,
+  where,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+
 function Dashboard() {
   const [user, loading] = useAuthState(auth);
   const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
   const navigate = useNavigate();
-  const fetchUserName = async () => {
+
+  const fetchUserData = async () => {
     try {
       if (user && user.uid) {
         const q = query(collection(db, "users"), where("uid", "==", user.uid));
-        const doc = await getDocs(q);
-        if (doc.docs.length > 0) {
-          const data = doc.docs[0].data();
-          setName(data.name);
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const userDoc = querySnapshot.docs[0];
+          const userData = userDoc.data();
+          setName(userData.name);
+          setBio(userData.bio);
         }
       }
     } catch (err) {
       console.error(err);
-      alert("An error occurred while fetching user data :(");
+      alert("An error occurred while fetching user data.");
     }
   };
 
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
-    fetchUserName();
+    fetchUserData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, loading, navigate]);
-  
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleBioChange = (event) => {
+    setBio(event.target.value);
+  };
+
+  const saveChanges = async () => {
+    try {
+      if (user && user.uid) {
+        const userRef = collection(db, "users");
+        const q = query(userRef, where("uid", "==", user.uid));
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          const userDoc = querySnapshot.docs[0];
+          const docRef = doc(db, "users", userDoc.id);
+          await updateDoc(docRef, {
+            name: name,
+            bio: bio,
+          });
+          alert("Changes saved successfully!");
+          setIsEditing(false);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      alert("An error occurred while saving changes.");
+    }
+  };
+
   return (
     <div className="dashboard">
       <div className="dashboard__container">
-        Logged in as
-        <div>{name}</div>
+        <div>
+          Logged in as: {name}
+        </div>
         <div>{user?.email}</div>
+        {isEditing ? (
+          <div>
+            <div className="dashboard__form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={handleNameChange}
+                placeholder="Enter your name"
+              />
+            </div>
+            <div className="dashboard__form-group">
+              <label htmlFor="bio">Bio:</label>
+              <textarea
+                id="bio"
+                value={bio}
+                onChange={handleBioChange}
+                placeholder="Enter your bio"
+              ></textarea>
+            </div>
+            <button className="dashboard__btn" onClick={saveChanges}>
+              Save Changes
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div>{bio}</div>
+            <button
+              className="dashboard__btn"
+              onClick={() => setIsEditing(true)}
+            >
+              Edit
+            </button>
+          </div>
+        )}
         <button className="dashboard__btn" onClick={logout}>
           Logout
         </button>
       </div>
     </div>
   );
-  
 }
+
 export default Dashboard;
